ba4a9e09558669684fe98a9a34dfccc6a53cdb03
Grant T. Aguinaldo <grant.aguinaldo@gmail.com>
2023-04-26 07:54:02 -0700

    updated files

--- example_model.py
+++ example_model.py
@@ -1,5 +1,11 @@
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory
+import argparse
+
+parser = argparse.ArgumentParser(description='Solve Simple Model.')
+parser.add_argument('--solver', type=str, required=True, metavar='', help='Solver')
+args = parser.parse_args()
+solver = args.solver
 
 model = pyo.ConcreteModel()
 
@@ -15,5 +21,5 @@ model.profit = pyo.Objective(
 model.demand = pyo.Constraint(expr = model.x <= 40)
 model.laborA = pyo.Constraint(expr = model.x <= 80)
 model.laborB = pyo.Constraint(expr = 2*model.x <= 100)
-opt = pyo.SolverFactory('cplex').solve(model, tee=True)
+opt = pyo.SolverFactory(solver).solve(model, tee=True)
 model.display()
